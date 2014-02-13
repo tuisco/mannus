aab6e966c4fe0e9f29b6f7ef0e2d364a0d567f4e
Matthew Gradidge + Philip Lewis Cameron Stevens <devs+mg+plcs@gmail.com>
2014-02-13 11:35:43 +0000

    Working implementation of http/get to reevoo api and rendering a view.

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [http-kit "2.1.16"]]
+                 [http-kit "2.1.16"]
+                 [clj-jade "0.1.4"]]
+  :main mannus.handler
   :plugins [[lein-ring "0.8.10"]]
-  :ring {:handler mannus.handler/app}
+  :ring {:handler mannus.handler/app
+         :auto-reload? true
+         :auto-refresh? true}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
--- src/mannus/handler.clj
+++ src/mannus/handler.clj
@@ -1,10 +1,33 @@
 (ns mannus.handler
   (:use compojure.core)
+  (:use ring.middleware.reload)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [org.httpkit.client :as http]
+            [clj-jade.core :as jade]))
+
+(jade/configure {:template-dir "views/"
+                 :pretty-print true
+                 :cache? true})
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" {{trkref :trkref sku :sku} :params}
+    ((def options {:timeout 200  ; ms
+                  :basic-auth ["" ""]
+                  :query-params {:param "value" :param2 ["value1" "value2"]}
+                }
+    )
+    (http/get (str "http://localhost:3001/v4/organisations/" trkref "/reviewable.json?sku=" sku) options
+              (fn [{:keys [status headers body error]}] ;; asynchronous handle response
+                (if error
+                  (println "Failed, exception is " error)
+                  (println "Async HTTP GET: " body))))
+    (http/get (str "http://localhost:3001/v4/organisations/" trkref "/locale/en-GB/reviews.json?sku=" sku) options
+              (fn [{:keys [status headers body error]}] ;; asynchronous handle response
+                (if error
+                  (println "Failed, exception is " error)
+                  (println "Async HTTP GET: " body)))))
+    (jade/render "index.jade" {}))
   (route/resources "/")
   (route/not-found "Not Found"))
 
