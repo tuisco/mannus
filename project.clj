f1dc2d3de67a451cfff606adcafabe91065d8b51
Philip Stevens <philip.stevens@reevoo.com>
2014-02-13 10:02:20 +0000

    Add http-kit.

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [http-kit "2.1.16"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler mannus.handler/app}
   :profiles
